20bc1ec945f77ffcdfe5785c072ae3cccc0acb11
Egemen <egemenutku.kus@ogr.deu.edu.tr>
2023-03-03 04:18:04 +0300

    Added test to state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-
 pub struct State {
     accounts: HashMap<String, Account>,
 }
@@ -16,15 +15,24 @@ impl State {
     }
 
     pub fn get_balance(&self, address: String) -> &u64 {
-        &self.accounts.get(&address).unwrap().balance
+        match self.accounts.get(&address) {
+            Some(account) => &account.balance,
+            None => &0,
+        }
     }
 
     pub fn get_nonce(&self, address: String) -> &u64 {
-        &self.accounts.get(&address).unwrap().nonce
+        match self.accounts.get(&address) {
+            Some(account) => &account.nonce,
+            None => &0,
+        }
     }
 
-    pub fn get_address(&self, address: String) -> &String {
-        &self.accounts.get(&address).unwrap().address
+    pub fn get_address(&self, address: String) -> &str {
+        match self.accounts.get(&address) {
+            Some(account) => &account.address,
+            None => "0",
+        }
     }
 }
 
@@ -32,4 +40,34 @@ struct Account {
     balance: u64,
     nonce: u64,
     address: String,
+}
+
+#[cfg(test)]
+
+mod tests {
+    use crate::{helpers::new_random_wallet};
+    use super::State;
+
+
+    #[test]
+    fn test_get_account() {
+        let wallet = new_random_wallet();
+        assert_eq!(wallet.get_address().len(),64);
+    }
+    #[test]
+    fn test_balance() {
+        let wallet = new_random_wallet();
+        let address = wallet.get_address().to_string();
+        let state = State::new();
+        let balance = *state.get_balance(address);
+        assert_eq!(balance,0);
+    }
+    #[test]
+    fn test_address() {
+        let wallet = new_random_wallet();
+        let address = wallet.get_address().to_string();
+        let state = State::new();
+        let address_func = state.get_address(address);
+        assert_eq!(address_func,"0");
+    }
 }
